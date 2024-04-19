6a3e4261e632f8010b736fc93f48dd4febc9cb5b
MatthewDaPew <162397704+MatthewDaPew@users.noreply.github.com>
2024-04-20 10:43:35 +1200

    mad libs.py
    the funny message

---
+++ mad libs.py
@@ -0,0 +1,12 @@
+print("Hello, this is your mo- computer speaking. Fill out dis form please.")
+word = input("What is your name? ")
+if word == "nothing":
+    print("Thats fire!!!!!!")
+else:
+    print("Thats fire!!!!!!!")
+print("You know what else is fire? ")
+deez = input("XBOX LIVEEEEEEEEE!!!!! But you can tell me what you think. ")
+if deez == "nothing":
+    print("Thats an interesting choice. But anyways, bye bye.")
+else:
+    print("Hmmm ok...")
